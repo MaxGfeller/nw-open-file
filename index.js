e3d001ae6851e37b88a13d73ca848ba27e5bc915
Max Gfeller <max.gfeller@gmail.com>
2014-10-08 13:01:52 -0600

    Remove input element after
    Clean up the DOM after the element is no more used so it doesn't get in
    the way.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var cuid = require('cuid')
 
 module.exports = function(cb) {
-    var el = document.createElement('input')
-    el.type = 'file'
-    el.style.display = 'none'
-    document.body.appendChild(el)
-    el.addEventListener('change', function(evt) {
-        cb(this.value)
-    })
-    el.click()
+	var el = document.createElement('input')
+	el.type = 'file'
+	el.style.display = 'none'
+	document.body.appendChild(el)
+	el.addEventListener('change', function(evt) {
+			document.body.removeChild(el)
+			cb(this.value)
+	})
+	el.click()
 }
